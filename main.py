05a3e7c3beee8cb199eada58a3fbdb809711c73a
Alex_ <90696382+Plai83@users.noreply.github.com>
2024-01-27 15:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
 for name_col in set(lst):
-    att_temp = [1 if z == name_col else 0 for z in data['whoAmI'].tolist()]
-    data[name_col] = att_temp
+    arr_temp = [1 if z == name_col else 0 for z in data['whoAmI'].tolist()]
+    data[name_col] = arr_temp
 
 new_df = data[list(set(lst))]
 print(new_df)
